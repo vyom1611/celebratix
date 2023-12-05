931de973a0522fa4524a7f9f53f69a5c8b7ae1f1
vyom1611 <sharm843@umn.edu>
2023-12-05 15:20:59 -0600

    Added Venue Queries

---
+++ CelebraTix.Promotions/Venues/VenueQueries.cs
@@ -0,0 +1,47 @@
+using CelebraTix.Promotions.Promotions.Data;
+using CelebraTix.Promotions.Venues;
+using Microsoft.EntityFrameworkCore;
+
+public class VenueQueries
+{
+    private readonly PromotionDataContext context;
+
+    public VenueQueries(PromotionDataContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<List<VenueInfo>> ListVenues()
+    {
+        var venues = await context.Venues
+            .Select(venue => new
+            {
+                venue.VenueGuid,
+                Description = venue.Descriptions.OrderByDescending(d => d.ModifiedDate).FirstOrDefault()
+            })
+            .ToListAsync()
+            .ConfigureAwait(false);
+
+        return venues.Select(row => MapVenue(row.VenueGuid, row.Description)).ToList();
+    }
+
+    public async Task<VenueInfo> GetVenue(Guid venueGuid)
+    {
+        var venue = await context.Venues
+            .Where(venue => venue.VenueGuid == venueGuid)
+            .Select(venue => new { venue.VenueGuid })
+            .SingleOrDefaultAsync()
+            .ConfigureAwait(false);
+
+        return venue == null ? null : MapVenue(venue.VenueGuid, null);
+    }
+
+    private VenueInfo MapVenue(Guid venueGuid, VenueDescription venueDescription) 
+        => new VenueInfo
+        {
+            VenueGuid = venueGuid,
+            Name = venueDescription?.Name,
+            City = venueDescription?.City,
+            LastModifiedTicks = venueDescription?.ModifiedDate.Ticks ?? 0
+        };
+}
