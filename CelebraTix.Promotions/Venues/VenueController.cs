c33cbc41824c9e6ad6753efb23f01c3022c4819a
vyom1611 <sharm843@umn.edu>
2023-12-07 02:24:56 -0600

    VenueController amended with the addition of VenueViewModel

--- CelebraTix.Promotions/Venues/VenueController.cs
+++ CelebraTix.Promotions/Venues/VenueController.cs
@@ -1,45 +1,26 @@
-using CelebraTix.Promotions.Venues;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CelebraTix.Promotions.Venues;
 
-/// <summary>
-/// Controller for managing venues in the CelebraTix application.
-/// Provides actions for listing, creating, editing, and deleting venues.
-/// </summary>
 public class VenuesController : Controller
 {
     private readonly VenueQueries queries;
     private readonly VenueCommands commands;
 
-    /// <summary>
-    /// Constructor for VenuesController.
-    /// </summary>
-    /// <param name="queries">Service for querying venue data.</param>
-    /// <param name="commands">Service for executing commands on venue data.</param>
     public VenuesController(VenueQueries queries, VenueCommands commands)
     {
         this.queries = queries;
         this.commands = commands;
     }
 
-    /// <summary>
-    /// Displays a list of all venues.
-    /// </summary>
-    /// <returns>A view displaying a list of venues.</returns>
+    // GET: Venues
     public async Task<IActionResult> Index()
     {
-        var venues = await queries.ListVenues();
-        return View(venues);
+        return View(await queries.ListVenues());
     }
 
-    /// <summary>
-    /// Displays details of a specific venue.
-    /// </summary>
-    /// <param name="id">The unique identifier of the venue.</param>
-    /// <returns>A view showing the details of a venue.</returns>
+    // GET: Venues/Details/abc-123
     public async Task<IActionResult> Details(Guid id)
     {
         var venue = await queries.GetVenue(id);
@@ -51,49 +32,41 @@ public class VenuesController : Controller
         return View(venue);
     }
 
-    /// <summary>
-    /// Displays a form for creating a new venue.
-    /// Redirects to a filled form if an id is provided.
-    /// </summary>
-    /// <param name="id">Optional unique identifier for pre-filling the form (if exists).</param>
-    /// <returns>A view containing the form for creating a venue.</returns>
+    // GET: Venues/Create
+    // GET: Venues/Create/abc-123
     public IActionResult Create(Guid? id)
     {
         if (id == null)
         {
-            return View(new VenueInfo());
-        }
-        else
-        {
             return RedirectToAction(nameof(Create), new { id = Guid.NewGuid() });
         }
+
+        var viewModel = new VenueViewModel
+        {
+            TimeZones = AllTimeZones
+        };
+        return View(viewModel);
     }
 
-    /// <summary>
-    /// Handles the submission of a new venue.
-    /// </summary>
-    /// <param name="id">The unique identifier for the new venue.</param>
-    /// <param name="venue">The venue data bound from the form submission.</param>
-    /// <returns>Redirects to the index view on success, or redisplays the form on failure.</returns>
+    // POST: Venues/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create(Guid id, [Bind("Name,City")] VenueInfo venue)
+    public async Task<IActionResult> Create(Guid id, [Bind("Venue")] VenueViewModel viewModel)
     {
         if (ModelState.IsValid)
         {
+            var venue = viewModel.Venue;
             venue.VenueGuid = id;
             await commands.SaveVenue(venue);
             return RedirectToAction(nameof(Index));
         }
 
-        return View(venue);
+        return View(viewModel);
     }
 
-    /// <summary>
-    /// Displays a form for editing an existing venue.
-    /// </summary>
-    /// <param name="id">The unique identifier of the venue to edit.</param>
-    /// <returns>A view containing the form for editing a venue.</returns>
+    // GET: Venues/Edit/abc-123
     public async Task<IActionResult> Edit(Guid id)
     {
         var venue = await queries.GetVenue(id);
@@ -102,34 +75,33 @@ public class VenuesController : Controller
             return NotFound();
         }
 
-        return View(venue);
+        var viewModel = new VenueViewModel
+        {
+            Venue = venue,
+            TimeZones = AllTimeZones
+        };
+        return View(viewModel);
     }
 
-    /// <summary>
-    /// Handles the submission of venue edits.
-    /// </summary>
-    /// <param name="id">The unique identifier of the venue being edited.</param>
-    /// <param name="venue">The venue data bound from the form submission.</param>
-    /// <returns>Redirects to the index view on success, or redisplays the form on failure.</returns>
+    // POST: Venues/Edit/abc-123
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(Guid id, [Bind("Name,City")] VenueInfo venue)
+    public async Task<IActionResult> Edit(Guid id, [Bind("Venue")] VenueViewModel viewModel)
     {
         if (ModelState.IsValid)
         {
+            var venue = viewModel.Venue;
             venue.VenueGuid = id;
             await commands.SaveVenue(venue);
             return RedirectToAction(nameof(Index));
         }
 
-        return View(venue);
+        return View(viewModel);
     }
 
-    /// <summary>
-    /// Displays a confirmation page for venue deletion.
-    /// </summary>
-    /// <param name="id">The unique identifier of the venue to delete.</param>
-    /// <returns>A view asking for confirmation to delete a venue.</returns>
+    // GET: Venues/Delete/abc-123
     public async Task<IActionResult> Delete(Guid id)
     {
         var venue = await queries.GetVenue(id);
@@ -141,11 +113,7 @@ public class VenuesController : Controller
         return View(venue);
     }
 
-    /// <summary>
-    /// Handles the confirmed deletion of a venue.
-    /// </summary>
-    /// <param name="id">The unique identifier of the venue to delete.</param>
-    /// <returns>Redirects to the index view on successful deletion.</returns>
+    // POST: Venues/Delete/abc-123
     [HttpPost, ActionName("Delete")]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> DeleteConfirmed(Guid id)
@@ -153,4 +121,12 @@ public class VenuesController : Controller
         await commands.DeleteVenue(id);
         return RedirectToAction(nameof(Index));
     }
+
+    private static List<SelectListItem> AllTimeZones =>
+        TimeZoneInfo.GetSystemTimeZones()
+            .Select(timeZone => new SelectListItem
+            {
+                Value = timeZone.Id,
+                Text = $"{timeZone.DisplayName} ({timeZone.Id})"
+            }).ToList();
 }
